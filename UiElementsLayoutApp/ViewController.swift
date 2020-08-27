44a5b1b6b8ede70c17d1defb2fce5395a40a12dc
agsaule <agsaule@hotmail.com>
2020-08-27 10:45:15 +0300

    new project

--- UiElementsLayoutApp/ViewController.swift
+++ UiElementsLayoutApp/ViewController.swift
@@ -10,11 +10,67 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var leftButtonUp: UIButton!
+    @IBOutlet weak var rightButtonUp: UIButton!
+    @IBOutlet weak var leftButtonDown: UIButton!
+    @IBOutlet weak var rightButtonDown: UIButton!
+    
+    @IBOutlet var styleOutletCollection: [UIButton]!
+    
+    @IBOutlet weak var myLable: UILabel!
+    
+    @IBOutlet weak var mainImageView: UIImageView!
+    
+    
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+       }
+    
+func styleUI(){
+    mainImageView.layer.cornerRadius = 10
+    mainImageView.layer.borderWidth = 2
+    mainImageView.layer.borderColor = UIColor.black.cgColor
+    
+    myLable.layer.cornerRadius = 10
+    myLable.layer.borderWidth = 2
+    myLable.layer.borderColor = UIColor.black.cgColor
+    
+    
+    styleOutletCollection.forEach { button in
+        button.layer.cornerRadius = 10
+        button.layer.borderWidth = 2
+        button.layer.borderColor = UIColor.black.cgColor
     }
-
-
 }
 
+//MARK:- Logic Style to myLable, Buttons and Image
+    
+    func changeButtonImage(with image: String){
+        leftButtonUp.setImage(UIImage(named: "home.png"), for: .normal)
+        leftButtonUp.tintColor = .white
+        leftButtonUp.imageEdgeInsets = UIEdgeInsets(top: 8, left: 2, bottom: 10, right: 15)
+        self.view.backgroundColor = UIColor.black
+    }
+    
+    func changeTextLable(with message: String){
+        let text = message
+        myLable.text = text
+        myLable.backgroundColor = UIColor.gray
+        mainImageView.image = UIImage(systemName: "mic")
+        
+        
+    }
+    
+//MARK:- IBAction for 3 more buttons
+    @IBAction func touchDragOutsideTapped(_ sender: Any) {
+        print("touchDragOutsideTapped")
+        changeButtonImage(with: "home.png")
+    }
+    
+    @IBAction func leftBottomButtonTapped(_ sender: Any) {
+        changeTextLable(with: "leftBottomButtonTapped")
+    }
+    
+    
+}
